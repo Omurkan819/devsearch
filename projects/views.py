abc07065aa60b07721888e1b4d77508d1180157c
Omurkan819 <oabaskanov@mail.ru>
2022-08-12 04:05:33 +0600

    first commit

---
+++ projects/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This was a project where I built out my portfolio'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Awesome open source project I am still working'
+    }
+]
+
+def projects(request):
+    context = { 'projects':projectsList}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    return render(request, 'projects/single-project.html', {'pagekey': pk})
